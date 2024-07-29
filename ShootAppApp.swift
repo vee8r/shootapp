748c8226ab3a29635520328e127824a39f6cb492
kuzio <weronikka004@gmail.com>
2024-07-29 10:35:38 +0200

    first commit

---
+++ ShootAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShootAppApp.swift
+//  ShootApp
+//
+//  Created by Weronika Kuzio on 29/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShootAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
